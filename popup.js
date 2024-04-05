11b5a22bca7db08ad4746b1683adb7a97e6002ae
Muhammad Fahad <rfgul587@gmail.com>
2024-04-05 10:42:07 +0500

    trying to add new feature of commenting as well.

--- popup.js
+++ popup.js
@@ -5,9 +5,31 @@ document.addEventListener("DOMContentLoaded", function () {
         target: { tabId: tabs[0].id },
         func: () => {
           document.querySelectorAll("#thumbnail > yt-image > img")[0].click();
-          console.log("clicked");
+
+          var commentBtn = document.getElementById("comment");
+
+          commentBtn.addEventListener("click", function () {
+            console.log("comment button clicked");
+            var placeHolder = document.querySelector("#placeholder-area");
+
+            placeHolder.click();
+            placeHolder.focus();
+            placeHolder.execCommand("insertText", true, "hello");
+
+            var button = document.getElementById("submit-button");
+
+            if (button) {
+              button.click();
+            } else {
+              console.log("Button with ID 'submit-button' not found.");
+            }
+          });
         },
       });
     });
   });
 });
+
+// // click button id is submit-button
+// document.querySelector('#placeholder-area').focus()
+// document.execCommand('insertText', true, 'hello' )
